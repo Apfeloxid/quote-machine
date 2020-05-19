0b2ff613fb1d96961807246123b8574a2f383746
romberch <christopher.romberg@student.uni-augsburg.de>
2020-05-19 05:15:30 +0200

    Updates

---
+++ precache-manifest.f1cf4f07e009975a6e05d96191bc31c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1f0094ece10cb3b529c057d93973e5",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "3733c2b0f606344c5a38",
+    "url": "/quote-machine/static/css/main.1a6388a3.chunk.css"
+  },
+  {
+    "revision": "67e2dfb7a2986c8769dc",
+    "url": "/quote-machine/static/js/2.36631971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.36631971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3733c2b0f606344c5a38",
+    "url": "/quote-machine/static/js/main.0737e661.chunk.js"
+  },
+  {
+    "revision": "cbc9f74f0ef17ee9ad82",
+    "url": "/quote-machine/static/js/runtime-main.6752162c.js"
+  }
+]);
